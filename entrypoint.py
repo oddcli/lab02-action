9b28f042ff5bb4227ca760136ca2d0bf1a3c0ff6
oddcli <112713950+oddcli@users.noreply.github.com>
2022-12-25 10:28:38 -0300

    Create entrypoint.py

---
+++ entrypoint.py
@@ -0,0 +1,48 @@
+import json
+import os
+import io
+import paramiko
+
+
+def deploy(vps_info, username, command, ssh_key):
+    try:
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        if ssh_key:
+            pkey = paramiko.RSAKey.from_private_key(io.StringIO(ssh_key))
+            ssh_client.connect(vps_info['address'], port=vps_info['port'], pkey=pkey, username=username)
+        else:
+            ssh_client.connect(vps_info['address'], port=vps_info['port'], password=vps_info['pwd'], username=username)
+    except Exception as error:
+        print(error.message)
+    try:
+        stdin, stdout, stderr = ssh_client.exec_command(command)
+        for line in stdout:
+            print(line)
+    except Exception as error:
+        print(error.message)
+
+if __name__ == '__main__':
+
+    vps_list_file = os.environ["VPS_LIST_FILE"]
+    deploy_command_file = os.environ["DEPLOY_COMMAND"]
+    ssh_key = os.environ["SSH_KEY"]
+    username = os.environ["USERNAME"]
+
+    try:
+        with open(vps_list_file, 'r') as f:
+            vps_list = json.load(f)
+    except:
+        print("Error reading VPS list file")
+
+    try:
+        if os.path.isfile(deploy_command_file):
+            with open(deploy_command_file, 'r') as f:
+                deploy_command = f.read()
+    except:
+        print("Error reading deploy command file")
+
+    for vps_info in vps_list:
+        deploy(vps_info, username, deploy_command, ssh_key)
+        
