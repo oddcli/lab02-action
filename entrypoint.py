498c3f2062cfe8d1a1c93f4ad63cc91a617d9e53
oddcli <112713950+oddcli@users.noreply.github.com>
2022-12-25 15:58:30 -0300

    v1 working, so fresh so clean

--- entrypoint.py
+++ entrypoint.py
@@ -87,9 +87,9 @@ if __name__ == '__main__':
             print(f"[!] Erro na thread id: {id}, VPS: {vps_info['address']}:{vps_info['port']}, Erro: {e}")
         
         if not result.exception():
-            print(f"Exito ao executar o comando no VPS: {vps_info['address']}:{vps_info['port']}, Thread id: {id}, {result.state}")
+            print(f"Exito ao executar o comando no VPS: {vps_info['address']}:{vps_info['port']}, Thread id: {id}, {result}")
         else:
-            print(f"[!] Erro ao executar o comando no VPS: {vps_info['address']}:{vps_info['port']}, Thread id: {id}, {result.state}")
+            print(f"[!] Erro ao executar o comando no VPS: {vps_info['address']}:{vps_info['port']}, Thread id: {id}, {result}")
         
     
     ### POC end
