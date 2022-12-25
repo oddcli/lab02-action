9555bef4c63175d937e62d9b036da578d4c4c881
oddcli <112713950+oddcli@users.noreply.github.com>
2022-12-25 15:15:57 -0300

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -44,14 +44,6 @@ def deploy(vps_info, username, command, ssh_key):
     except Exception as e:
         print(e.message)
 
-"""
-def listar_metodos(objeto):
-    # Lista todos os elementos (métodos e atributos) da instância
-    todos_elementos = dir(objeto)
-    # Filtra a lista para incluir apenas os métodos
-    metodos = [elem for elem in todos_elementos if callable(getattr(objeto, elem))]
-    return metodos
-"""
 
 if __name__ == '__main__':
 
@@ -87,6 +79,7 @@ if __name__ == '__main__':
         except Exception as error:
             # Captura qualquer exceção lançada pela função deploy
             print(f"[!] Erro ao executar o comando no VPS: {vps_info['address']}, Thread id: {id} {error}")
+            raise error
         else:
             print(f"Exito ao executar o comando no VPS: {vps_info['address']}, Thread id: {id} {result}")
      ### POC end
